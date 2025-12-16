aaf086450699ca2408b1f8417e0f50c839491f9a
dmasliuk <dmasliuk.w@gmail.com>
2025-12-16 10:46:04 -0700

    added base window and game speed config

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while 1:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +18,7 @@ def main():
                 return
         screen.fill("#000000")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
