864e391844b610a1b7f8f48c7a8bc673634d5c32
dmasliuk <dmasliuk.w@gmail.com>
2025-12-16 10:31:26 -0700

    added logger

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while 1:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#000000")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
