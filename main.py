2494ce8d2d9376aca0fba3783a1650d50b1e6bf9
dmasliuk <dmasliuk.w@gmail.com>
2025-12-16 20:20:55 -0700

    added asteroids class, asteroids spawn and move logic

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 
@@ -15,8 +17,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while 1:
         log_state()
@@ -24,8 +31,6 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("#000000")
-
-        
         for obj in drawable:
             obj.draw(screen)
         updatable.update(dt)
